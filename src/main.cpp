a7fb3452247a2877d1fa151dc7df1e23693a536b
VanNamond <2376784637@qq.com>
2023-05-26 10:35:40 +0800

    44

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+/* // #include <iostream>
+// #include<string.h>
+// using namespace std;
+// int toUnicode(const char* str)
+// {
+// 	return str[0] + (str[1] ? toUnicode(str + 1) : 0);
+// }
+// constexpr inline int U(const char* str)
+// {
+// 	return str[0] + (str[1] ? U(str + 1) : 0);
+// }
+// int main(int argc, char *argv[])
+// {
+//     bool a=true;
+//     int b=a;
+//     cout<<b;
+//     // string a;
+//     // cin>>a;
+//     // const char* b;
+//     // b=a.data();
+
+//     // while(1){
+//     //     switch (toUnicode(b))
+//     //     {
+//     //     case U("login"):
+//     //         cout<<"hello";
+//     //         cout<<"aaa";
+//     //         break;
+//     //     case U("debuff"):
+//     //         cout<<"sorry";
+//     //         cout<<"No";
+//     //     default:
+//     //         break;
+//     //     }
+//     //     cin>>a;
+//     // }
+
+// }
+
+ */
